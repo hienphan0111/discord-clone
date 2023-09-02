00eaf7bbcc115c8a10b2fd80aef0bf75cba64776
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-09-02 13:06:58 +0700

    Add upload file

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { X } from "lucide-react";
+import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
-
+import '@uploadthing/react/styles.css';
+import { split } from "postcss/lib/list";
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
@@ -9,10 +12,25 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split('.').pop();
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="upload"
+          className="rounded-full"
+        />
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone 
       endpoint={endpoint}
-      onClientUploadComplete={(res) => onChange(res?.[0].fileUrl)}
+      onClientUploadComplete={(res) => onChange(res?.[0].url)}
       onUploadError={(err) => console.log(err)}
     />
   )
