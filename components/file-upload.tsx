ba787cec9615350c622228aa00bcf363f652d14d
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-09-06 11:40:28 +0700

    completed create a server

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -23,6 +23,13 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
           alt="upload"
           className="rounded-full"
         />
+        <button
+          onClick={() => onChange()}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+            <X className="h-4 w-4" />
+          </button>
       </div>
     )
   }
@@ -31,7 +38,7 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     <UploadDropzone 
       endpoint={endpoint}
       onClientUploadComplete={(res) => onChange(res?.[0].url)}
-      onUploadError={(err) => console.log(err)}
+      onUploadError={(err) => console.log("err")}
     />
   )
 }
